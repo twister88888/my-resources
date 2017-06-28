e0a10e8a5038379a0e7b50d92c50613c2e77a0f2
twister88888 <twister88888@gmail.com>
2017-06-28 21:15:00 +0200

    Added glyphicons fonts

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,9 +3,18 @@ import React from 'react';
 const Item = props => {
   return (
     <div className='Item'>
-        <input className='Item__check' type='checkbox' onClick={props.handleCheckedClick}/>
         <span className='Item__date'>{props.date}</span>
         <a className='Item__name' href={props.link} target='_blank' rel='noopener noreferrer'>{props.name}</a>
+        <a className='Item__link' href={props.link} target='_blank' rel='noopener noreferrer'>{props.link}</a>
+        <div className='Item__buttons'>
+          <a className='Item__favourites'>
+            <span className='glyphicon glyphicon-star-empty' aria-hidden='true'></span>
+          </a>
+          <a className='Item__favourites' onClick={props.handleCheckedClick}>
+            <span className='glyphicon glyphicon glyphicon-ok' aria-hidden='true'></span>
+          </a>
+          {/*<input className='Item__check' type='checkbox' onClick={props.handleCheckedClick}/>*/}
+        </div>
     </div>
   )
 }
